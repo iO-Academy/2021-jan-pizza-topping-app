42557647e8e4c5e3059b47167186866043fed288
Karen Schulz <sirk.schulz@gmail.com>
2021-04-26 16:44:51 +0100

    added <Header /> and erased <App>

--- pizza-app-frontend/src/index.js
+++ pizza-app-frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Components/Header/Header.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
