9c4aad8010cd332ac8e1d062967117abce8e5736
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-27 11:42:37 +0100

    Added fetch statement to correct port (3000) and imported node into react

--- pizza-frontend/src/service/service.js
+++ pizza-frontend/src/service/service.js
@@ -1,7 +1,7 @@
-export async function getAllPizzas() {
+export default async function getAllPizzas() {
 
     try{
-        const response = await fetch('/'); //localhost
+        const response = await fetch('http://localhost:3001/');
         return await response.json();
     } catch(error) {
         console.log('Failed to return response.json')
