d746aa5d7bd1e71c2e821d7096c411ccfcdd9e43
Karen Schulz <sirk.schulz@gmail.com>
2021-04-27 11:50:00 +0100

    created wrapper

---
+++ pizza-app-frontend/src/Components/Card1/Wrapper.js
@@ -0,0 +1,12 @@
+// import React from 'react';import './wrapper.css';
+//
+// class Wrapper extends React.Component {
+//     render() {
+//         return (
+//             <main className="mainWrapper">
+//                 {this.props.children}
+//             </main>
+//         )
+//     }
+// }
+// export default Wrapper
