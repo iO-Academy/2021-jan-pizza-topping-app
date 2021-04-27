65f611c81b61e1ed09bd306f0cd008eb61dc0bf2
Karen Schulz <sirk.schulz@gmail.com>
2021-04-27 09:14:00 +0100

    created file, imported and exported stuff

---
+++ pizza-app-frontend/src/Components/Card1/Card1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card1.css'
+
+class Card1 extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Card1
