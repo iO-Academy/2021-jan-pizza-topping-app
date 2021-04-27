ce6a0562969867e1469e238a58e062a4baa7d77f
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-27 13:49:19 +0100

    commented out array of pizzas in favor of the random array

--- pizza-app-backend/index.js
+++ pizza-app-backend/index.js
@@ -16,11 +16,18 @@ app.get('/', function(request, response) {
         console.log('Connected')
         const pizzaCollection = client.db('pizza-app').collection('pizza-toppings')
         const individualPizza = await pizzaCollection.find({}).toArray()
+        const results = [];
+            for (let i = 0; i < individualPizza.length - 1; i++) {
+              for (let j = i + 1; j < individualPizza.length; j++) {
+                results.push(`${individualPizza[i]} ${individualPizza[j]}`);
+              }
+            }
 
         response.send({
             success: true,
-            message: 'Successfully retrieved all the individual pizzas',
-            data: individualPizza
+            message: 'Successfully retrieved all the randomised pizzas',
+//            arrayOfPizzas: individualPizza
+            arrayOfShuffledPizzas: results
         })
     })
 })
@@ -30,8 +37,3 @@ app.listen(port, () => {
 })
 
 
-//export async function getAllPizzas() {
-//
-//    const response = await fetch('/');
-//    return await response.json();
-//}
