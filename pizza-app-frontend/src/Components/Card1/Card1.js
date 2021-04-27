f70e15a7066ace56303c14263858333721d8e2ee
Karen Schulz <sirk.schulz@gmail.com>
2021-04-27 11:47:08 +0100

    added some content

--- pizza-app-frontend/src/Components/Card1/Card1.js
+++ pizza-app-frontend/src/Components/Card1/Card1.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './card1.css'
+import Wrapper from "./Wrapper";
 
 class Card1 extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <div>
-
+<div>
+            <div className="card1">
+                <p>blablah</p>
             </div>
+    <div className="card2">
+        <p>whwhwhwhwhw whwhwhw</p>
+    </div>
+</div>
+
         )
     }
 }
