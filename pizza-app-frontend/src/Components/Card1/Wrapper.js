37633a7d095997cf9538646d15f76a061aa15b1b
Karen Schulz <sirk.schulz@gmail.com>
2021-04-27 13:56:10 +0100

    added a warppaer

--- pizza-app-frontend/src/Components/Card1/Wrapper.js
+++ pizza-app-frontend/src/Components/Card1/Wrapper.js
@@ -1,12 +1,12 @@
-// import React from 'react';import './wrapper.css';
-//
-// class Wrapper extends React.Component {
-//     render() {
-//         return (
-//             <main className="mainWrapper">
-//                 {this.props.children}
-//             </main>
-//         )
-//     }
-// }
-// export default Wrapper
+import React from 'react';import './wrapper.css';
+
+class Wrapper extends React.Component {
+    render() {
+        return (
+            <main className="mainWrapper">
+                {this.props.children}
+            </main>
+        )
+    }
+}
+export default Wrapper
