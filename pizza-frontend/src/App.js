b23585146a07dc391658429db5630c7ff5399ebd
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-27 13:47:22 +0100

    changed display to show random array of objects

--- pizza-frontend/src/App.js
+++ pizza-frontend/src/App.js
@@ -7,38 +7,19 @@ class Pizzas extends React.Component{
     constructor(props){
     super(props);
     this.state = {
-        pizzaData: getAllPizzas()
+        randomisedArray: getAllPizzas()
     }
    }
+this.randomisedArray
+
+
     render(){
     return(
         <div>
-                Pizza Data: {this.state.getAllPizzas}
+                <p>Pizza Random Array: {this.randomisedArray()}</p>
         </div>
     )
     }
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default Pizzas;
