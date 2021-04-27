be910cafcebe81098d12f49f70f202b7f58d1d3b
Karen Schulz <sirk.schulz@gmail.com>
2021-04-27 19:53:45 +0100

    added a <span>, simplified className system

--- pizza-app-frontend/src/Components/Card/Card.js
+++ pizza-app-frontend/src/Components/Card/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './card.css'
 import Wrapper from "../Wrapper";
 
+
 class Card extends React.Component {
     constructor(props) {
         super(props);
@@ -13,18 +14,20 @@ class Card extends React.Component {
 
                 <div className="card1">
                     <img src="/images/mushrooms.jpg"/>
-                    <div className="textNameCard1">
-                        <p>Name: blablabla</p>
-                        <p>Category:</p>
+                    <div className="textCard1">
+                        <p className="name">Name: blablabla</p>
+                        <p className="category">Category:</p>
+                        <span className="dot1">1</span>
                     </div>
                 </div>
 
                 <div className="card2">
                     <img src="/images/aubergine.jpg"/>
-                    <div className="textNameCard2">
-                        <p>Name: blablabla</p>
-                        <p>Category:</p>
+                    <div className="textCard2">
+                        <p className="name">Name: blablabla</p>
+                        <p className="category">Category:</p>
                     </div>
+                    <span className="dot2">2</span>
                 </div>
 
 
