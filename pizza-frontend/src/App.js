5e3731b1a6d707846dee60742b05fe9365f6a227
JoJo <62075451+CaffeinatedCoder91@users.noreply.github.com>
2021-04-28 09:35:29 +0100

    add App.js frontend to git

--- pizza-frontend/src/App.js
+++ pizza-frontend/src/App.js
@@ -18,7 +18,7 @@ class PizzasApi extends React.Component{
  callApi() {
         fetch('http://localhost:3000/')
             .then(res  => res.text())
-            .then(res => this.setState({ apiResponse: res }));
+            .then(res => this.setState({ apiResponse: res }))
     }
 
     componentWillMount() {
