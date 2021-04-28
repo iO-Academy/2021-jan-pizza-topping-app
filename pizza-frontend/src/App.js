309ff5fcaafe90b71fffb454b9993411cbf8fc53
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-28 14:20:02 +0100

    Made changes on Mike's recommendation

--- pizza-frontend/src/App.js
+++ pizza-frontend/src/App.js
@@ -7,32 +7,56 @@ class PizzasApi extends React.Component{
     constructor(props){
     super(props);
     this.state = {
-        apiResponse: "",
-
+        apiResponse: [],
+        clickCounter: -1,
+        results: []
      };
    }
 
  callApi() {
         fetch('http://localhost:3000/')
-            .then(res  => res.text())
-            .then(res => this.setState({ apiResponse: res }))
+            .then(res  => res.json())
+            .then(res => {this.setState({
+            apiResponse: res
+            })
+            console.log(this.state.apiResponse)
+            })
             .catch(err => err);
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.callApi();
     }
 
 
-    render() {
-       return(
+populateResults = ()=>{
+    let resultsArray = [];
+     for (let i = 0; i < this.state.apiResponse.length - 1; i++) {
+        for (let j = i + 1; j < this.state.apiResponse.length; j++) {
+        let obj1= JSON.stringify(this.state.apiResponse[i]);
+        let obj2 = JSON.stringify(this.state.apiResponse[j]);
+            resultsArray.push(`${obj1}${obj2}`);
+           }
+     }
+         this.setState({
+            clickCounter: this.state.clickCounter+1,
+            results: resultsArray
+         })
+     }
 
-           <div>
-                <p>Pizza Random Array:  {JSON.stringify(this.state.apiResponse)} </p>
-           </div>
-       )
-   }
+    render(){
+    console.log(this.state.apiResponse)
+        return(
+            <div>
+                <p>clickCounter: {this.state.clickCounter}</p>
+               <p> results: {this.state.results}</p>
+               <p>Two Randoms: {this.state.results[this.state.clickCounter]}</p>
+               <p><button onClick={this.populateResults} >Click here to begin</button></p>
+            </div>
+        )
+    }
 }
+
 
 export default PizzasApi;
 
