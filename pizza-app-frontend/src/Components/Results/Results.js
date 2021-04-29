2ce74a66d2d84482489ace6e8d3e45ccfe7a9d3e
Karen Schulz <sirk.schulz@gmail.com>
2021-04-29 18:27:26 +0100

    made the page neater

--- pizza-app-frontend/src/Components/Results/Results.js
+++ pizza-app-frontend/src/Components/Results/Results.js
@@ -11,18 +11,17 @@ class Results extends React.Component {
                 <div className="bigCard">
                     <h1 className="h1results">Pizza App</h1>
                     <h2 className="h2results">the top 10 toppings are:</h2>
-                    <ol className="list">
-                        <li>blah</li>
-                        <li>bllaaaaa</li>
-                        <li>luuuuuu</li>
-                        <li>blah</li>
-                        <li>bllaaaaa</li>
-                        <li>luuuuuu</li>
-                        <li>blah</li>
-                        <li>bllaaaaa</li>
-                        <li>luuuuuu</li>
-                        <li>agggggg</li>
-                    </ol>
+                        <p className="list">1. Tomatoes</p>
+                        <p className="list">2. Mozzarella</p>
+                        <p className="list">3. Chorizo</p>
+                        <p className="list">4. Mushrooms</p>
+                        <p className="list">5. Tandoori Chicken</p>
+                        <p className="list">6. Rocket</p>
+                        <p className="list">7. Prosciutto</p>
+                        <p className="list">8. Pulled pork</p>
+                        <p className="list">9. Peppers</p>
+                        <p className="list">10. Sweetcorn</p>
+
 </div>
             </div>
         )
