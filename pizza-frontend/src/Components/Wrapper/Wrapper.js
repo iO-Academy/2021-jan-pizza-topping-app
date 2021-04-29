30d9bab6e6d3d5802fab913499d55134f8209629
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-29 18:02:04 +0100

    Added wrapper file to pizza-frontend from pizza-app-frontend

---
+++ pizza-frontend/src/Components/Wrapper/Wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './wrapper.css';
+
+class Wrapper extends React.Component {
+    render() {
+        return (
+            <main className="mainWrapper">
+                {this.props.children}
+            </main>
+        )
+    }
+}
+export default Wrapper;
