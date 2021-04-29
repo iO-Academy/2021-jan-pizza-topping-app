72a2b127233d7b12a814195ab2eeaebbcbf89e99
Karen Schulz <sirk.schulz@gmail.com>
2021-04-29 16:54:30 +0100

    deleted app junk and copy-pasted code

--- pizza-app-frontend/src/App.js
+++ pizza-app-frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const shuffle = require('shuffle-array')
+class PizzasApi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: [],
+      clickCounter: -1,
+      results: [],
+      pizzaObjectArray: []
+    };
+    this.callApi()
+  }
+  callApi() {
+    fetch('http://localhost:3001/')
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            apiResponse: res
+          })
+          console.log(this.state.apiResponse)
+          this.populateResults()
+        })
+        .catch(err => err);
+  }
+  setShuffleArray = () => {
+    this.setState({
+          shuffleArray: shuffle(this.state.pizzaObjectArray),
+        }
+    )
+  }
+  populateResults = () => {
+    this.setState({
+      pizzaObjectArray: this.state.apiResponse.arrayOfPizzas
+    })
+    this.setShuffleArray()
+  }
+  render(){
+    console.log(this.state.pizzaObjectArray[0])
+    if("shuffleArray" in this.state)
+    {return(
+        <div>
+          {this.state.shuffleArray.slice(0, 2).map(pizza => {
+            return(
+                <div>
+                  <p> {pizza.toppingsname} </p>
+                  <p><img src={pizza.toppingsimg} /> </p>
+                </div>
+            )
+          })}
+        </div>
+    )}
+    else { return <div> </div> }
+  }
 }
-
-export default App;
+export default PizzasApi;
