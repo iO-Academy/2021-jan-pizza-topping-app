48196ba518944690547a90791a7c48671adfb98a
JoJo <62075451+CaffeinatedCoder91@users.noreply.github.com>
2021-04-29 10:04:49 +0100

    refactored the code to put this.callAPI in constructor, and map function to display the pizza

--- pizza-frontend/src/App.js
+++ pizza-frontend/src/App.js
@@ -2,59 +2,59 @@ import React from 'react';
 import './App.css';
 const shuffle = require('shuffle-array')
 
-class PizzasApi extends React.Component{
-    constructor(props){
-    super(props);
-    this.state = {
-        apiResponse: [],
-        clickCounter: -1,
-        results: [],
-        pizzaObjectArray: []
-     };
-   }
 
- callApi() {
+class PizzasApi extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiResponse: [],
+            clickCounter: -1,
+            results: [],
+            pizzaObjectArray: []
+        };
+        this.callApi()
+    }
+
+    callApi() {
         fetch('http://localhost:3001/')
-            .then(res  => res.json())
-            .then(res => {this.setState({
-            apiResponse: res
-            })
-            console.log(this.state.apiResponse)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    apiResponse: res
+                })
+                console.log(this.state.apiResponse)
+                this.populateResults()
             })
             .catch(err => err);
     }
 
-    componentDidMount() {
-        this.callApi();
-    }
-
-
-    blah = () => {
+    setShuffleArray = () => {
         this.setState({
-          shuffleArray: shuffle(this.state.pizzaObjectArray),
-          }
+                shuffleArray: shuffle(this.state.pizzaObjectArray),
+            }
         )
-     }
+    }
 
     populateResults = () => {
         this.setState({
             pizzaObjectArray: this.state.apiResponse.arrayOfPizzas
         })
-        this.blah()
+        this.setShuffleArray()
     }
-
 
 
     render(){
         console.log(this.state.pizzaObjectArray[0])
+        // if("shuffleArray" in this.state)
         return(
 
             <div>
-                <p>{JSON.stringify(this.state.pizzaObjectArray.categoriesid)}</p>
-                <p>{JSON.stringify(this.state.pizzaObjectArray[1])}</p>
-                <button onClick={ this.populateResults }> Click me </button>
+                {this.state.shuffleArray.map(pizza => {
+                    return <p> {pizza.categoriesname} </p>
+                })}
             </div>
         )
+        // else { return <div> </div> }
     }
 }
 
