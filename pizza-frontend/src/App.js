bbfba43234c19249b9e8892122745ce16e805859
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-29 16:34:42 +0100

    Added fetch request to update db on every click

--- pizza-frontend/src/App.js
+++ pizza-frontend/src/App.js
@@ -47,17 +47,35 @@ class PizzasApi extends React.Component {
          [pizza.toppingsname]: this.state[pizza.toppingsname]+1 || 1,
          clickCounter: this.state.clickCounter+1 || 1
          })
-//put[pizza.toppingsname]
-//        method: 'PUT'[this.state[pizza.toppingsname]]
-        this.updateVotes()
+         this.setShuffleArray()
+
+        async function updatePost (pizza) {
+                         const requestOptions = {
+                             method: 'PUT',
+                             headers: { 'Content-Type': 'application/json' },
+                             body: JSON.stringify({ pizza })
+                         };
+                         const response = await fetch('http://localhost:3001/:categoriesid', requestOptions);
+                         const data = await response.json();
+                     }
+//fetch request
+//to send stuff to the database
+
     }
 
 //     updateVotes = ()=>{
-//     this.setState({
-//
-//     })
-//      scoreBoard: this.state.scoreBoard + [[pizza.toppingsname] => [this.state.pizza.toppingsname]]
-//        }
+//        async function updatePost () {
+//                const requestOptions = {
+//                    method: 'PUT',
+//                    headers: { 'Content-Type': 'application/json' },
+//                    body: JSON.stringify({ pizza })
+//                };
+//                const response = await fetch('http://localhost:3001/:categoriesid', requestOptions);
+//                const data = await response.json();
+//            }
+//            updatePost();
+//        };
+
 
     render(){
    //if(this.state.clickCounter < noOfCombos){ } else{<FinalPage />}
@@ -73,10 +91,12 @@ class PizzasApi extends React.Component {
                     <p> {pizza.toppingsname} </p>
                     <p><img src={pizza.toppingsimg} /> </p>
                     <p>This Item has {this.state.[pizza.toppingsname]} votes </p>
+                    <a href="http://localhost:3000/results">See final Results</a>
                     </div>
 
                     )
-                })}
+                })
+                }
 
             </div>
         )}
@@ -90,10 +110,11 @@ class PizzasApi extends React.Component {
 export default PizzasApi;
 
 
-
+//have it rerun render function when you've clicked
 //1. Send put request to the back end to update votes on db
 //2. find out how to show votes from db on page
 //3. complete if statement so that once you run out of images you get re-directed to the endpage
 //4. display items in order from most to least votes
 //5. styling
 
+//link to final page
