3cadbb5b2143c158a8c6a28699f5f1764889b7e5
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-04-29 18:09:55 +0100

    moved footer to pizza-frontend

---
+++ pizza-frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p className="pFooter">App designed by Jojo, Karen, Louise and Winter. &#169; 2021</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
