0bc5a38d1d0e7358ec1ff798771eef62ab00a9c8
Karen Schulz <sirk.schulz@gmail.com>
2021-04-29 16:21:52 +0100

    added results

---
+++ pizza-app-frontend/src/Components/Results/Results.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './results.css'
+import Footer from "../Footer/Footer";
+
+
+
+class Results extends React.Component {
+    render() {
+        return (
+            <div className="majorCard">
+                <div className="bigCard">
+                    <h1 className="h1results">Pizza App</h1>
+                    <h2 className="h2results">the top 10 toppings are:</h2>
+                    <ol className="list">
+                        <li>blah</li>
+                        <li>bllaaaaa</li>
+                        <li>luuuuuu</li>
+                        <li>blah</li>
+                        <li>bllaaaaa</li>
+                        <li>luuuuuu</li>
+                        <li>blah</li>
+                        <li>bllaaaaa</li>
+                        <li>luuuuuu</li>
+                        <li>agggggg</li>
+                    </ol>
+</div>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Results
+
